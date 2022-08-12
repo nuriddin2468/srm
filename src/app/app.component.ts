31b74e6f2b93509a8fcf4ee628a69c4bdce699df
Nuriddin_Yuldashev <Nuriddin_Yuldashev@epam.com>
2022-08-12 19:50:19 +0500

    git ignore updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from './core/services/database.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'personal';
+
+  constructor(
+    private db: DatabaseService
+  ) {}
+
+  ngOnInit() {
+    this.db.log();
+  }
 }
---
+++ src/app/core/services/database.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import PouchDB from 'pouchdb';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  private readonly db: PouchDB.Database;
+
+  constructor() {
+    this.db = new PouchDB('locale');
+  }
+
+  log(): void {
+    console.log(this.db);
+  }
+}
